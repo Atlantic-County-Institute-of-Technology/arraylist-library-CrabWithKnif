81f3e789dcdb80f96401086a1eba076b13aef05f
RKowerski <rileyko975@gmail.com>
2025-03-07 14:03:51 -0500

    03/07 - Final Commit
    Finished project and added comments

--- Library.java
+++ Library.java
@@ -1,41 +1,73 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+//Imports ArrayLists, Comparators, and Random
+//Comparators are used for .sort() and Random for shuffling
 
 public class Library {
+    //Private class variable for the song list
     private ArrayList<String> queue;
 
     public Library(){
+        //Class constructor, setting up a new ArrayList that can hold strings
         queue = new ArrayList<String>();
     }
 
     public void addItem(String item){
+        //Uses .add() to add a user submitted item
         queue.add(item);
     }
 
     public void removeItem(String item){
+        //Uses .remove() to remove a user submitted item
         queue.remove(item);
     }
 
     public String getNextItem(){
+        //Uses .getFirst() to show the user the first song in the list
         return queue.getFirst();
     }
 
     public void skipNextItem(){
+        //Uses .removeFirst() to "skip" the next song (item)
         queue.removeFirst();
     }
 
     public void sortAlphabetically(){
-        queue.sort(null);
+        //Uses the .sort() method of ArrayLists to compare strings s1 and s2. which are each
+        //one respective item. It then specifically compares s1 to s2, ignoring uppercase or lowercase
+        queue.sort((s1, s2) -> s1.compareToIgnoreCase(s2));
     }
 
-    public int findItem(String item){
-        /*if (!queue.contains(item))
-            return "No song with this name in queue.";
-        else*/
-            return queue.indexOf(item);
+    public ArrayList<String> shuffle(){
+        //This is almost the exact same shuffle method from my Game of Cut
+        Random rand = new Random();
+        //Makes a new random variable
+        for(int k = 0; k<=queue.size(); k++){
+            //Selects two random cards and swaps their position using a temporary variable
+            int random = rand.nextInt(queue.size());
+            int random2 = rand.nextInt(queue.size());
+            String temp = queue.get(random);
+            String temp2 = queue.get(random2);
+            //The main difference is, because ArrayLists are actually mutable, I can just use
+            //the .set() method to switch around spots.
+            queue.set(random, temp2);
+            queue.set(random2, temp);
+        }
+        return queue;
+    }
+
+    public Integer findItem(String item){
+        //Checks if the queue contains an item. If it does, return the position of that item in the queue.
+        //If it doesn't, return null so the main program can tell the user that the song doesnt exist.
+        if (!queue.contains(item))
+            return null;
+        else
+            return queue.indexOf(item)+1;
     }
 
     public String toString(){
+        //Makes a library return the same thing as an ArrayList would, instead of the memory address of the Library
         return queue.toString();
     }
 
--- Main.java
+++ Main.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
-
+//Imports the scanner class
 public class Main {
     public static void main(String[] args) {
+        //Initiates a new library for the song queue
         Library songList = new Library();
-        Scanner scan = new Scanner(System.in);
+        //Sets up a scanner for integers and a scanner for strings
+        Scanner scanI = new Scanner(System.in);
+        Scanner scanS = new Scanner(System.in);
+        //Set up a boolean variable to determine if the program should still be running.
+        //Will be set to false if the user chooses to quit
         boolean running = true;
-        songList.addItem("Just Lean");
-        songList.addItem("Plazma");
-        songList.addItem("a");
-        songList.addItem("b");
-        songList.addItem("c");
-        System.out.println(songList);
         while(running){
+            //Creates a menu for the user using print statements
             System.out.println("1. Play Song");
             System.out.println("2. Skip Song");
             System.out.println("3. Add Song");
@@ -22,19 +22,58 @@ public class Main {
             System.out.println("8. Find Song");
             System.out.println("0. Exit");
             System.out.println("Welcome to your queue! What would you like to do?");
-            int choice = scan.nextInt();
+            //Uses the int scanner to look for user choice
+            int choice = scanI.nextInt();
             if(choice==1) {
-                System.out.println("Now playing: " + songList.getNextItem());
+                //Creates a variable to hold the first song on the list, and then displays it to the user.
+                //Then, utilizes the skip code to pop it out of the list
+                String currentlyPlaying = songList.getNextItem();
+                System.out.println("Now playing: " + currentlyPlaying);
                 songList.skipNextItem();
             }
+            else if (choice==2){
+                //Exact same as above, but just implies skipping instead of playing
+                String skipped = songList.getNextItem();
+                System.out.println("You skipped: " + skipped);
+                songList.skipNextItem();
+            }
+            else if (choice==3){
+                //Lets the user add any string of text as a song
+                System.out.println("What song would you like to add?");
+                songList.addItem(scanS.nextLine());
+            }
+            else if (choice==4){
+                //Asks the user for a song name to remove, nd just moves on if there is no song by that name
+                System.out.println("What song would you like to remove (song name)?");
+                songList.removeItem(scanS.nextLine());
+            }
             else if (choice==5) {
+                //Prints the current queue
                 System.out.println(songList + "\n");
             }
             else if (choice==6) {
+                //Sorts the list alphabetically
                 songList.sortAlphabetically();
             }
+            else if (choice==7){
+                //Shuffles all current items in the list.
+                songList.shuffle();
+            }
+            else if (choice==8){
+                //Initiates an Integer variable to hold the song position, and then checks if it is null.
+                //If it returns null, tell the user that there is no song by that name
+                System.out.println("What song would you like to know the position of?");
+                Integer pos = songList.findItem(scanS.nextLine());
+                if (pos == null)
+                    System.out.println("That song is not in queue");
+                else
+                    System.out.println("That song is at position " + pos + " in queue.");
+            }
             else if (choice == 0)
+                //Sets the running variable to false, stopping the program
                 running = false;
+            else
+                System.out.println("Please choose an option!");
         }
     }
 }
